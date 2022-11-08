9d5cf80a6fb6e3e8e173900e8b2383ed9aa5df4a
Ollie <oliverwest06@outlook.com>
2022-11-08 19:42:55 +0100

    change

--- Python/Exo/Novembre-2022/07-11-22/Random_cube.py
+++ Python/Exo/Novembre-2022/07-11-22/Random_cube.py
@@ -1,22 +1,21 @@
-import turtle as t
 import random as r
+import turtle as t
 
 window = t.Screen()
-window.setup (width=2000, height=2000, startx=0, starty=0)
+window.setup(width=2000, height=2000, startx=0, starty=0)
 window.title(" 1ere - Turtle ")
 
 t.color("aqua")
-t.bgcolor("orangered")
+t.bgcolor("black")
 t.speed(0)
 t.penup()
 t.setposition(-300, -400)
 t.pendown()
 
 
-
 # taper votre code ci-dessous
-   
-a =  r.randint(10, 600) 
+
+a = r.randint(10, 600)
 for i in range(5):
     t.forward(a)
     t.left(90)
@@ -31,9 +30,6 @@ t.right(60)
 t.forward(a)
 t.left(60)
 t.forward(a/2)
-    
-
-
 
 
 # conserver pour la gestion de la fenêtre
