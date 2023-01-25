08cdc7706c87bb09e4254f343ecb80a6627375c7
TechieOllie <oliverwest06@outlook.com>
2023-01-25 20:40:43 +0100

    add responsiveness to image (still needs a bit of fine tuning

--- Web_Dev/js/scripts.js
+++ Web_Dev/js/scripts.js
@@ -1,3 +1,20 @@
 window.onload = function () {
   document.getElementById("loading-screen").style.display = "none";
 };
+
+var flash = document.getElementById("flash");
+
+setInterval(function () {
+  if (flash.style.color === "red") {
+    flash.style.color = "white";
+  } else {
+    flash.style.color = "red";
+  }
+}, 200);
+
+var viewportWidth = window.innerWidth;
+var viewportHeight = window.innerHeight;
+
+var img = document.getElementById("img-nantes");
+img.style.width = viewportWidth * 0.6 + "px";
+img.style.height = viewportHeight * 0.6 + "px";
